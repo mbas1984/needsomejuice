c63c358dd1bc5c2fd55a7ed3801a230e09031af9
mbas1984 <bsalisu1@live.maryville.edu>
2021-04-11 22:10:28 -0400

    project updated with Mongoose

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+const juiceRoutes = require('./routes/juiceUser');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(juiceRoutes);
+
+mongoose
+  .connect(
+    'mongodb+srv://juice:root@cluster0.ittcq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
